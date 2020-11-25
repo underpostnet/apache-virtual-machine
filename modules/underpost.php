f1fd232073647b0cd508f155cd5ac0f9ae05da9e
fcoverdugoa <fcoverdugoa@underpost.net>
2020-11-24 23:32:49 -0400

    renamed to underpost module

--- modules/underpost.php
+++ modules/underpost.php
@@ -11,7 +11,7 @@ function renderPath($uri, $str_data, $lang){
 
     if(($data['url'].$data['path'][$i]['url'])
             ==
-      (explode("?",("http://".explode("/", $data['url'])[2].$uri))[0])){
+      (explode("?",("https://".explode("/", $data['url'])[2].$uri))[0])){
 
       //------------------------------------------------------------------------
       //------------------------------------------------------------------------
@@ -109,7 +109,29 @@ function renderPath($uri, $str_data, $lang){
           <link rel='canonical' href='".$data['url'].$path['url']."' />
           <link rel='icon' type='image/png' href='".$data['url']."/assets/".$data['favicon']."' />
 
-          <!-- app assets -->
+          <link rel='apple-touch-icon' sizes='180x180' href='".$data['url']."/assets/app/apple-touch-icon.png'>
+          <link rel='icon' type='image/png' sizes='32x32' href='".$data['url']."/assets/app/favicon-32x32.png'>
+          <link rel='icon' type='image/png' sizes='16x16' href='".$data['url']."/assets/app/favicon-16x16.png'>
+
+          <link rel='icon' type='image/png' sizes='36x36' href='".$data['url']."/assets/app/android-chrome-36x36.png'>
+          <link rel='icon' type='image/png' sizes='48x48' href='".$data['url']."/assets/app/android-chrome-48x48.png'>
+          <link rel='icon' type='image/png' sizes='72x72' href='".$data['url']."/assets/app/android-chrome-72x72.png'>
+          <link rel='icon' type='image/png' sizes='96x96' href='".$data['url']."/assets/app/android-chrome-96x96.png'>
+          <link rel='icon' type='image/png' sizes='144x144' href='".$data['url']."/assets/app/android-chrome-144x144.png'>
+          <link rel='icon' type='image/png' sizes='192x192' href='".$data['url']."/assets/app/android-chrome-192x192.png'>
+          <link rel='icon' type='image/png' sizes='256x256' href='".$data['url']."/assets/app/android-chrome-256x256.png'>
+          <link rel='icon' type='image/png' sizes='384x384' href='".$data['url']."/assets/app/android-chrome-384x384.png'>
+
+          <link rel='icon' type='image/png' sizes='16x16' href='".$data['url']."/assets/app/favicon-16x16.png'>
+          <link rel='manifest' href='".$data['url']."/assets/app/site.webmanifest'>
+          <link rel='mask-icon' href='".$data['url']."/assets/app/safari-pinned-tab.svg' color='".$data['color']."'>
+
+          <meta name='apple-mobile-web-app-title' content='".$path['title'][$lang]."'>
+          <meta name='application-name' content='".$path['title'][$lang]."'>
+          <meta name='msapplication-config' content='".$data['url']."/assets/app/browserconfig.xml' />
+          <meta name='msapplication-TileColor' content='".$data['color']."'>
+          <meta name='msapplication-TileImage' content='".$data['url']."/assets/app/mstile-144x144.png'>
+          <meta name='theme-color' content='".$data['color']."'>
 
           <meta property='og:title' content='".$path['title'][$lang]."' />
           <meta property='og:description' content='".$path['description'][$lang]."' />
@@ -122,6 +144,21 @@ function renderPath($uri, $str_data, $lang){
           <meta name='viewport' content='width=device-width, user-scalable=no' />
 
           ".$global_css.$global_js."
+
+          <script src='https://static.addtoany.com/menu/page.js'></script>
+
+          <script src='https://unpkg.com/github-card@1.2.1/dist/widget.js'></script>
+
+          <script async src='https://www.googletagmanager.com/gtag/js?id=".$data['googletag']."'></script>
+
+          <script>
+
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '".$data['googletag']."');
+
+          </script>
 
         </head>
 

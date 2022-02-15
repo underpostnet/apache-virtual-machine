01b2fda2f1c727d256fd5c42758d14dcc1a19854
fcoverdugo <fcoverdugoa@underpost.net>
2022-02-15 01:41:11 -0300

    update

--- MAILER/modules/sendbymail.php
+++ MAILER/modules/sendbymail.php
@@ -4,7 +4,7 @@
 require("class.phpmailer.php");
 require("class.smtp.php");
 
-function mailSend($toemail, $name, $asunto, $msg){
+function mailSend($toemail, $name, $subject, $msg){
 
   $mail = new PHPMailer();
 
@@ -12,11 +12,11 @@ function mailSend($toemail, $name, $asunto, $msg){
 
   $mail->Host = "smtp.dondominio.com";  // specify main and backup server
   $mail->SMTPAuth = true;     // turn on SMTP authentication
-  $mail->Username = "contacto@somosindia.cl";  // SMTP username
-  $mail->Password = "somos-india-1A"; // SMTP password
+  $mail->Username = "";  // SMTP username
+  $mail->Password = ""; // SMTP password
 
   $mail->From = "contacto@somosindia.cl";
-  $mail->FromName = "Somos India";        // remitente
+  $mail->FromName = "";        // remitente
   $mail->AddAddress($toemail, $name);        // destinatario
 
   //$mail->AddReplyTo("tu-correo@tu-nombre-de-dominio.com", "respuesta a");    // responder a
@@ -24,7 +24,7 @@ function mailSend($toemail, $name, $asunto, $msg){
   $mail->WordWrap = 50;     // set word wrap to 50 characters
   $mail->IsHTML(true);     // set email
 
-  $mail->Subject = $asunto;
+  $mail->Subject = $subject;
   $mail->Body    = $msg;
   $mail->AltBody = "Somos India"; //no imprime...
 
